ad06defb9c8b9e725508fc07ccc2ed3e8a7a8cb5
Daniel Bergmann <danielbergmann1@gmail.com>
2014-10-28 20:20:39 +0000

    Added csv files and empty project

---
+++ TRG_project4.R
@@ -0,0 +1,6 @@
+############################################################################################
+#course:  Time Series Analysis, T-862-TIMA
+#Date:    //2014
+#Students: Daniel Bergmann Sigtryggsson, Lilja Bjorg Gudmundsdottir, Jon Vilberg Georgsson 
+#
+############################################################################################
