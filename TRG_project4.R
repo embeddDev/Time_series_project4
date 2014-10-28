dce2f8913c7fd6e07596b378adae44e30ca17be5
Daniel Bergmann <danielbergmann1@gmail.com>
2014-10-28 21:05:38 +0000

    las inn gognin og plottadi

--- TRG_project4.R
+++ TRG_project4.R
@@ -4,3 +4,22 @@
 #Students: Daniel Bergmann Sigtryggsson, Lilja Bjorg Gudmundsdottir, Jon Vilberg Georgsson 
 #
 ############################################################################################
+
+sat.dat <- read.csv("Satelliteorbit.csv",header=FALSE,col.names=c("rm","theta_m","V3"))
+sat.dat$rm[2] = sat.dat$theta_m[2]
+sat.dat$theta_m[2] = sat.dat$V3[2]
+sat.dat$V3 = NULL
+plot(y = sat.dat$rm * sin(sat.dat$theta_m),
+     x = sat.dat$rm * cos(sat.dat$theta_m),
+     type='l',
+     col=2,
+     lwd=3,
+     ylab = "y",
+     xlab = "x",
+     main = "Satellite measurement data")
+grid()
+ #TASK1 - State space model
+
+
+
+set.seed(1213)
