8d3c7a7005f67d7b2cea60611c2d9e759f88147a
Daniel Bergmann <danielbergmann1@gmail.com>
2014-11-08 11:37:11 +0000

    sync byrjun a fundi

--- TRG_project4_Part2.R
+++ TRG_project4_Part2.R
@@ -6,17 +6,19 @@
 ############################################################################################
 
 # ---------------- PART 2 ----------------
-
 dat <- rbind(read.csv2("Elspot2013.csv",encoding = "UTF-8"),read.csv2("Elspot2014.csv",encoding = "UTF-8"))
 dat$Time <- seq(as.POSIXct(as.character(dat$Date[1]),format="%e.%m.%Y",tz="CET")+3600,by=3600,length.out=nrow(dat))
-dat$Hour <- as.numeric(dat$Hour)
+dat$Hours <- as.numeric(dat$Hours)
+#dat$Price = as.numeric(dat$Price)
 dat$Date <- format(dat$Time,format="%Y-%m-%d")
 
-plot(dat$Price,
+plot(ts(dat$Price,start=c(2013,1),f=(365*24)),
      type = 'l',
      main='Price of Electricity',
      xlab='Time Index',
-     ylab='Price')
+     ylab='Price',
+     col="blue"
+     )
 # plot med godum toppi, viljum losna vid hann
 
 dat$Price[dat$Price > 600] <- 600
@@ -25,41 +27,58 @@ plot(dat$Price,
      type = 'l',
      main='Price of Electricity',
      xlab='Time Index',
-     ylab='Price')
+     ylab='Price',
+     col="blue")
 # litur ut fyrir ad vera nokkurn vegin stationary, notum recursive dot
 
-par(mfrow = c(2,1))
+layout(1:2)
 acf(dat$Price)
 pacf(dat$Price)
 # Greinilega eitthvad seasonal i tessu, notum (1,0,1)^24 og (1,0,0)^168 eins og gefid er i verkefni
 
 #R_null = (0 0 0 ; 0 0 0 0) (7x7 fylki af núllum)
+Rt 
 #R_einn er þá R_null + x_1*x^T
-#sum of squares , inniheldur þennan part af covariancinum
-#þeta(covariancinn held ég) =[ (x^T*X)^-1 ] *x^T*Y (á bladi)
+#sum of squares , inniheldur thennan part af covariancinum
+#theta(covariancinn held eg) =[ (x^T*X)^-1 ] *x^T*Y (á bladi)
 
 # Initialize-a X, fyrsta lina i X er (1 Wind(169) Consumtion(168) Price(168) Price(145) P(1) eps(145))
 
-búa til df sem inniheldur Wind(169) Consumtion(168) Price(168) Price(145) P(1) P(0)
+#bua til df sem inniheldur Wind(169) Consumtion(168) Price(168) Price(145) P(1) P(0)
 
-mdl <- lm(P(0) ~ Wind(169) + Consumtion(168) + Price(168) + Price(145) + P(1),x=T,y=T)
-
+# mdl <- lm(P(0) ~ dat$Wind(169) + 
+#             dat$Consumtion(168) + 
+#             dat$Price(168) +
+#             dat$Price(145) + 
+#             P(1)
+#           ,x=T,y=T)
+mdl <- lm(dat$Price ~ dat$Wind(169) + 
+            dat$Consumtion(168) + 
+            dat$Price(168) +
+            dat$Price(145),
+          x=T,y=T)
+#mdl = arma(dat$Price, order= c(2,0,0),
+#          seasonal = (list(order=c(1,0,1),period=24))) )
 X <- cbind(mdl$x,NA)
 Y <- mdl$y
 
 X[1:1000,7] <- mdl$residuals[1:1000]
 
-# Initialize-a Þetu, sem 0 vigur
+# Initialize-a th�etu, sem 0 vigur
 
 # Initialize-a R = zeros(7x7) matrix
 
 # gera recursive:
 # for t = 169:n
-# Reikna R(t) =  lambda*R(t-1) + x(t)*x(t)^T   byrja með lambda = 1, fá til að virka
-# Uppfæra þeta(þeta) = þeta(t-1)+R(t)^-1*x(t)*(Y(t)-X(t)^T*þeta(t-1)) setja try utanum if(class(þeta)!="try-error") ef þetta heldur þá þetta true og þá hægt að reikna restina af dótinu
-# Reikna Y(t+k|t) = Price(t) = langa jafnan
-# á morgun veit ég 
-# Uppfæra X, þe. uppfæra eps 
+for(t in 169:n){
+  # Reikna R(t) =  lambda*R(t-1) + x(t)*x(t)^T   byrja med lambda = 1, fa til ad virka
+  # Uppfaera theta(theta) = theta(t-1)+R(t)^-1*x(t)*(Y(t)-X(t)^T*theta(t-1)) setja try utanum if(class(þeta)!="try-error") ef þetta heldur þá þetta true og þá hægt að reikna restina af dótinu
+  # Reikna Y(t+k|t) = Price(t) = langa jafnan
+  # a morgun veit eg 
+  # Uppfaera X, þe. uppfaera eps   
+  
+}
+
 
 
 # nota fyrsta árið til að optimera lambda, prófa svo það lambda á seinna árið. 
