f6e411d52dc86c4765be79c9776fc7f466fd7b37
Daniel Bergmann <danielbergmann1@gmail.com>
2014-11-12 10:48:02 +0000

    attempt at forecasting 13 hours ahead

--- TRG_project4_Part2.R
+++ TRG_project4_Part2.R
@@ -24,7 +24,7 @@ plot(dat$Price,
      xlab='Time Index',
      ylab='Price',
      col="blue"
-     )
+)
 # plot med godum toppi, viljum losna vid hann
 
 dat$Price[dat$Price > 600] <- 600
@@ -49,19 +49,12 @@ nSamp = 1000
 #mdl <- lm(rep(1,(nSamp-168)) ~ dat$Wind[169:nSamp] +
 #Xvar = data.frame(price = dat$Price, W169 = dat$Wind,L169=dat$Consumption,P168=lag(dat$Price,1),P145=lag(dat$Price,24),P1=lag(dat$Price,168))
 Xvar2013 = data.frame(price = dat$Price[169:(nSampYear)], 
-                  W169 = dat$Wind[169:nSampYear],
-                  L169=dat$Consumption[169:nSampYear],
-                  P168=dat$Price[168:(nSampYear-1)],
-                  P145=dat$Price[145:(nSampYear-24)],
-                  P1=dat$Price[1:(nSampYear-168)])
+                      W169 = dat$Wind[169:nSampYear],
+                      L169=dat$Consumption[169:nSampYear],
+                      P168=dat$Price[168:(nSampYear-1)],
+                      P145=dat$Price[145:(nSampYear-24)],
+                      P1=dat$Price[1:(nSampYear-168)])
 mdl2013 <- lm(price ~ W169+L169+P168+P145+P1,data=Xvar2013,x=T,y=T)
-Xvar = data.frame(price = dat$Price[169:(nSamp)], 
-                      W169 = dat$Wind[169:nSamp],
-                      L169=dat$Consumption[169:nSamp],
-                      P168=dat$Price[168:(nSamp-1)],
-                      P145=dat$Price[145:(nSamp-24)],
-                      P1=dat$Price[1:(nSamp-168)])
-mdl <- lm(price ~ W169+L169+P168+P145+P1,data=Xvar,x=T,y=T)
 Y = as.matrix(mdl2013$y)
 acf(mdl2013$residuals)
 pacf(mdl2013$residuals)
@@ -78,47 +71,80 @@ err = c(mdl$residuals, rep(0,(length(Pt)-length(mdl$residuals))))
 err = as.numeric(err)
 XX = cbind(P0,Wt,Lt,Pt168,Pt24,Pt1,err)
 
-# Initialize-a th?etu, sem 0 vigur
-theta = matrix(0,7,1)
-# Initialize-a R = zeros(7x7) matrix
-Rt = matrix(0,7,7)
-n = nrow(XX)-1
-# Initialize empty matix and vector for storing
-theta.store <- matrix(NA,nrow(XX),7)
-det.store = matrix(NA,nrow(XX),1)
-yhat <- rep(NA,nrow(XX))
-lmbd <- 1
-# gera recursive:
-# for t = 169:n
-for(tt in 1:n){  
-  
-  xt <- XX[tt,]
-  #Update Rt
-  Rt <- xt%*%t(xt) + lmbd*Rt
-  det.store[tt] = det(Rt)
-  # Use "try" until Rt becomes invertible
-  theta.try <- try(theta + solve(Rt)%*%xt%*%(Y[tt] - t(xt)%*%theta),silent=T)
-  if(class(theta.try) != "try-error"){
-    #Update theta
-    theta <- theta.try
-    #Store
-    theta.store[tt,] <- theta
-    #Predict
-    yhat[tt+1] <- t(XX[(tt+1),]) %*% theta
-    XX[tt+1,7] = (Pt[tt+1]- yhat[tt+1]) 
+recurse_est = function(XX,Y,lmbd){
+  # Initialize-a th?etu, sem 0 vigur
+  theta = matrix(0,7,1)
+  # Initialize-a R = zeros(7x7) matrix
+  Rt = matrix(0,7,7)
+  n = nrow(XX)-1
+  # Initialize empty matix and vector for storing
+  theta.store <- matrix(NA,nrow(XX),7)
+  det.store = matrix(NA,nrow(XX),1)
+  yhat <- rep(NA,nrow(XX))
+  # gera recursive:
+  # for t = 169:n
+  for(tt in 1:n){  
+    
+    xt <- XX[tt,]
+    #Update Rt
+    Rt <- xt%*%t(xt) + lmbd*Rt
+    det.store[tt] = det(Rt)
+    # Use "try" until Rt becomes invertible
+    theta.try <- try(theta + solve(Rt)%*%xt%*%(Y[tt] - t(xt)%*%theta),silent=T)
+    if(class(theta.try) != "try-error"){
+      #Update theta
+      theta <- theta.try
+      #Store
+      theta.store[tt,] <- theta
+      #Predict
+      yhat[tt+1] <- t(XX[(tt+1),]) %*% theta
+      XX[tt+1,7] = (Pt[tt+1]- yhat[tt+1]) 
+    }
   }
+  yhat[yhat > 600]= 600
+  return(yhat)  
 }
+RMSE = function(sim,obs){
+  RMSE = mean( (sim[!is.na(sim)] - obs[!is.na(sim)])^2)
+}
+layout(1)
 
-# nota fyrsta arid til ad optimera lambda, profa svo that lambda a seinna arid. 
+lambda = 0.3
+ y = NA
+while(lambda <= 1){
+  y_mdl = recurse_est(XX,Y,lambda)
+  y = c(y,RMSE(y_mdl,Y))
+  lambda = lambda + 0.05
+}
+barplot(y[2:length(y)],ylab="RMSE",xlab="lambda, 0.3 to 1 increments of 0.05", col=2,main="simulation for different lambdas")
+grid()
+#Looks as though no forgetting factor gives least RMSE, going for 0.8 because it is plausible that it will improve the model
 
-
-
-
-
-
-
-
-
-
-
+nSamp = 1000
+nSampYear = length(dat2014$Price)-168
+Xvar = data.frame(price = dat2014$Price[169:(nSamp)], 
+                  W169 = dat2014$Wind[169:nSamp],
+                  L169=dat2014$Consumption[169:nSamp],
+                  P168=dat2014$Price[168:(nSamp-1)],
+                  P145=dat2014$Price[145:(nSamp-24)],
+                  P1=dat2014$Price[1:(nSamp-168)])
+mdl2014 <- lm(price ~ W169+L169+P168+P145+P1,data=Xvar,x=T,y=T)
+Y = matrix(dat2014$Price[169:nSampYear],byrow=T)
+Pt = dat2014$Price[169:nSampYear]
+Pt1 = dat2014$Price[(169-1):(nSampYear-1)]
+Lt = dat2014$Consumption[169:nSampYear]
+Pt24 = dat2014$Price[ (169-24) :(nSampYear-24)]
+Pt168 = dat2014$Price[1:(nSampYear - 168)]
+Wt = dat2014$Wind[169:nSampYear]
+P0 = rep(1,(nSampYear-168))
+err = c(mdl$residuals, rep(0,(length(Pt)-length(mdl$residuals))))
+err = as.numeric(err)
+XX = cbind(P0,Wt,Lt,Pt168,Pt24,Pt1,err)
+forecast_xx = matrix(rep(XX[6817,],times=17),nrow=17,ncol=7,byrow=T)
+XX = rbind(XX, forecast_xx)
+Y_forecast = matrix(rep(Y[length(Y)], times=17,nrow=17,ncol=1,byrow=T))
+Y = rbind(Y,Y_forecast)
+yhat2014 = ts(recurse_est(XX,Y,0.8),start=c(2014,1),frequency=24*365)
+plot(,type='l',col=2,ylab="Spot Price")
+grid()
 
